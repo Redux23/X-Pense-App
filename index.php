9e9fd80377be924befc4528cdd53c4f859918d91
Bright Osuagwu <bo.osuagwu@gmail.com>
2021-02-10 01:40:24 +0000

    Second commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Xpense App</title>
+</head>
+<body>
+    <h1>This is the sample page</h1>
+    <p>please rename your files fo instance: sample.php and not sample.html</p>
+
+    <?php
+
+    echo "This is a test page!!";
+    ?>
+</body>
+</html>
