4e2d7a60d8e4f7bc9e5b75b1208afd07e4e17027
Bright Osuagwu <bo.osuagwu@gmail.com>
2021-02-12 09:19:31 +0000

    Added pictures to the images folder

---
+++ layout.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>X-Pense App</title>
+</head>
+<body>
+    <header class="header">
+    <div>
+    <img src="" alt="X-Pense App">
+    </div>
+    <nav>
+    <ul>
+    <li>Home</li>
+    <li>About</li>
+    <li>Contact Us</li>
+    </ul>
+    </nav>
+    </header>
+    <main>
+    
+  
+   
+    
+    </main>
+    <footer>
+    
+    </footer>
+
+    <?php
+   
+   ?>
+</body>
+</html>
