c5ceb45aa33d739a01bb12e1444ed6f228ec4bab
Bright Osuagwu <bo.osuagwu@gmail.com>
2021-02-17 03:16:38 +0000

    Created DB connection

--- signin.php
+++ signin.php
@@ -69,7 +69,7 @@
     </footer>
 
     <?php
-
+   
   
    ?>
 </body>
--- signup.php
+++ signup.php
@@ -37,7 +37,7 @@
                 <input type="text" placeholder="Lastname" name="lastname"><br><br>
                 <input type="email" placeholder="Email" name="email"><br><br>
                 <input type="password" placeholder="Password" name="password"><br><br>
-                <input type="password" placeholder="Confirm Password" name="Confirm-password"><br><br>
+                <input type="password" placeholder="Confirm Password" name="confirm-password"><br><br>
                 <input type="submit" value="Sign up" name="SignUp-button">
                 
             </form>
@@ -77,6 +77,42 @@
 
     <?php
 
+     //Creating a connection to our database
+     function createNewUser(){
+        $servername = 'localhost';
+        $username = 'root';
+        $password = '';
+        $databasename = 'test_DB';
+   
+        $connection = mysqli_connect($servername, $username, $password, $databasename);
+   
+       //check if the connection was successful
+       if(!$connection){
+           die ('Connection unsuccessful: '.mysqli_connect_error());
+       }else{
+           echo 'Connection Success!';
+       }
+        //Storing user input into variables
+
+        $userName = $_POST['username'];
+        $firstName = $_POST['firstname'];
+        $lastName = $_POST['lastname'];
+        $emailName = $_POST['email'];
+        $password = $_POST['password'];
+        $confirmPassword = $_POST['confirm-password'];
+
+        //INSERT data into our database
+
+        $sql = "INSERT INTO test_DB (username, firstName, lastName, email, password, dateCreated)";
+        
+       
+
+       }
+   
+       //Create new user function call
+      if(isset($_POST['SignUp-button'])){
+          createNewUser();
+      }
    ?>
 </body>
 </html>
