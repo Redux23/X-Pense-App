1bb9d4aead982ca9c58e0026282af4f015ec2a28
oluwafunmilola99 <akinosofunmilola@gmail.com>
2021-03-03 08:47:34 +0000

    commit

--- signin.php
+++ signin.php
@@ -12,7 +12,15 @@
 </head>
 
 <body>
-    </a></li>
+    <header class="header">
+
+        <div class="images">
+            <img src="assets/images/xpenseLogo.png" class="logo" alt="X-Pense App">
+        </div>
+
+        <nav class="navbar">
+            <ul class="nav-links">
+                <li><a href href="#">About</a></li>
                 <li><a href="#">Contact Us</a></li>
             </ul>="index.php">Home</a></li>
                 <li><a>
@@ -22,15 +30,7 @@
 
 
     <main class="container">
-<header class="header">
 
-        <div class="images">
-            <img src="assets/images/xpenseLogo.png" class="logo" alt="X-Pense App">
-        </div>
-
-        <nav class="navbar">
-            <ul class="nav-links">
-                <li><a href href="#">About
             <!-- Write your code here -->
             <section class="form-wrapper-2">
                 <h4 class="form-info"> kindly Login to access your account</h4>
