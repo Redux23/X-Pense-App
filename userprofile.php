95a2114ffa0f0465810c5af4237fd652169a6cdb
oluwafunmilola99 <akinosofunmilola@gmail.com>
2021-03-03 23:37:34 +0000

    commit

---
+++ userprofile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=Z, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css" />
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet">
+    <title>X-Pense App</title>
+</head>
+<body>
+    <header class="header">
+        <div class="images">
+            <img src="assets/images/xpenseLogo.png" class="logo" alt="X-Pense App">
+        </div>
+            <nav class="navbar">
+                <ul class="nav-links">  
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Contact Us</a></li>
+                </ul>
+            </nav>
+    </header>
+    
+    
+</body>
+</html>
