fe160deb754f6d1c9c6c289c7cf5c6c690a760f4
oluwafunmilola99 <akinosofunmilola@gmail.com>
2021-03-03 00:36:00 +0000

    checkout_b userprofile layout_feat

--- userpage layout.php
+++ userpage layout.php
@@ -18,9 +18,14 @@
         <img src="assets/images/xpenseLogo.png" class="logo" alt="X-Pense App">
     </div>
 
-    
+    <nav class="navbar">
+        <ul class="nav-links">
+             <li><a href href="#">About</a></li>
+             <li><a href="#">Contact Us</a></li>
+        </ul>="index.php">Home</a></li>
+            <li><a>
+     </nav>
 
-
-
+</header>
 </body>
 </html>
