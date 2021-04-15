438ab56d6e3e0dfb0a75c50e5a13c58e931a6188
oluwafunmilola99 <akinosofunmilola@gmail.com>
2021-04-15 14:07:19 +0100

    weekly expense table

--- dashboard/monthlyexp.php
+++ dashboard/monthlyexp.php
@@ -64,7 +64,7 @@
 
             <!-- Nav Item - Tables -->
             <li class="nav-item">
-                <a class="nav-link" href="tables.html">
+                <a class="nav-link" href="weekly.php">
                     <i class="fas fa-fw fa-table"></i>
                     <span>Weekly Expenses</span></a>
             </li>
--- dashboard/weekly.php
+++ dashboard/weekly.php
@@ -29,16 +29,16 @@
 
     <!-- Page Wrapper -->
     <div id="wrapper">
-<!-- Sidebar -->
-<ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+    <!-- Sidebar -->
+    <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-<!-- Sidebar - Brand -->
-<a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
-    <div class="sidebar-brand-icon rotate-n-15">
+    <!-- Sidebar - Brand -->
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+        <div class="sidebar-brand-icon rotate-n-15">
         <i class="fas fa-laugh-wink"></i>
-    </div>
-    <div class="sidebar-brand-text mx-3">Welcome User</div>
-</a>
+        </div>
+        <div class="sidebar-brand-text mx-3">Welcome User</div>
+    </a>
 
 <!-- Divider -->
 <hr class="sidebar-divider my-0">
@@ -60,28 +60,28 @@
 
 <!-- Daily Expense Interface  -->
 <li class="nav-item">
-    <a class="nav-link" href="tables.html">
+    <a class="nav-link" href="dailytable.php">
         <i class="fas fa-fw fa-table"></i>
         <span>Daily Expenses</span></a>
 </li>
 
 <!-- Nav Item - Tables -->
 <li class="nav-item">
-    <a class="nav-link" href="tables.html">
+    <a class="nav-link" href="weekly.php">
         <i class="fas fa-fw fa-table"></i>
         <span>Weekly Expenses</span></a>
 </li>
 
 <!-- Nav Item - Tables -->
 <li class="nav-item">
-    <a class="nav-link" href="tables.html">
+    <a class="nav-link" href="monthlyexp.php">
         <i class="fas fa-fw fa-table"></i>
         <span>Monthly Expenses</span></a>
 </li>
 
 <!-- Nav Item - Charts -->
 <li class="nav-item">
-    <a class="nav-link" href="charts.html">
+    <a class="nav-link" href="charts.php">
         <i class="fas fa-fw fa-chart-area"></i>
         <span>Charts</span></a>
 </li>
